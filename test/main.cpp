40c6f6cb2747ad63a5c72eb0d1f9a8d337935892
kaochu <3510144038@qq.com>
2023-02-08 15:35:45 +0800

    test

--- test/main.cpp
+++ test/main.cpp
@@ -1,98 +1,59 @@
 #include<iostream>
 #include"tree.h"
-
-
-void sushu()
-{
-	double a;
-	float b;
-	int c;
-	cout << "请依次输入a,b,c" << endl;
-	cin >> a;
-	cin >> b;
-	cin >> c;
-	
-	double mix;
-	mix = a + b + c;
-	cout << mix << endl;
-
-}
-
-void shuzu()
-{
-	int tree[] = { 11,2,34,6,23,66,14,77,55,35 };
-
-
-		for (int i = 0; i < 9; i++)
-		{
-			for(int j=1;j<9-i;j++)
-			 {
-				if (tree[j] < tree[j - 1])
-				{
-				 int temp;
-				 temp = tree[j];
-				 tree[j ] = tree[j-1];
-				 tree[j - 1] = temp;
-				}
-			}
-		}
-		for (int i = 0; i < 9; i++)
-		{
-			cout << tree[i] << endl;
-		}
-}
-
-void koujuebiao()
-{
-	for (int i = 1; i < 10; i++)
-	{
-		for (int j = 1; j <=i; j++)
-		{
-			cout << i <<"*" <<j <<"="<<(j*i) << " ";
-		}
-		cout << endl;
-	}
-}
-
-void shuixianhua()
-{
-	int i = 100;
-	while (i < 10000)
-	{
-		int a=0;
-		int b=0;
-		int c=0;
-
-		a = i % 10;
-		b = i / 10 % 10;
-		c = i / 100;
-
-		if (a* a* a + b * b * b + c * c * c == i)
-			cout << i << endl;
-			i++;
-
-		
-
-
-	}
-}
-
-void hanoi()
-{
-
-}
+#include<vector>
 
 int main()
 {
-	//sushu();//素数
+	sushu();//素数
 	
 	//shuzu();//冒泡排序
 
 	//koujuebiao();//乘法口诀表
 
-	shuixianhua();//水仙花数
+	//shuixianhua();//水仙花数
 
+	/*int count;
+	cout << "请输入盘子数量：" << endl;
+	cin >> count;
+	hanoi(count, 'a', 'b', 'c');*/
+
+	//cout << isPalindrome(234) << endl;
+
+	
 	system("pause");
 	return 0;
 
 }
+
+/*
+1 /**
+2  *                             _ooOoo_
+3  *                            o8888888o
+4  *                            88" . "88
+5  *                            (| -_- |)
+6  *                            O\  =  /O
+7  *                         ____/`---'\____
+8  *                       .'  \\|     |//  `.
+9  *                      /  \\|||  :  |||//  \
+10  *                     /  _||||| -:- |||||-  \
+11  *                     |   | \\\  -  /// |   |
+12  *                     | \_|  ''\---/''  |   |
+13  *                     \  .-\__  `-`  ___/-. /
+14  *                   ___`. .'  /--.--\  `. . __
+15  *                ."" '<  `.___\_<|>_/___.'  >'"".
+16  *               | | :  `- \`.;`\ _ /`;.`/ - ` : | |
+17  *               \  \ `-.   \_ __\ /__ _/   .-` /  /
+18  *          ======`-.____`-.___\_____/___.-`____.-'======
+19  *                             `=---='
+20  *          ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+21  *                     佛祖保佑        永无BUG
+22  *            佛曰:
+23  *                   写字楼里写字间，写字间里程序员；
+24  *                   程序人员写程序，又拿程序换酒钱。
+25  *                   酒醒只在网上坐，酒醉还来网下眠；
+26  *                   酒醉酒醒日复日，网上网下年复年。
+27  *                   但愿老死电脑间，不愿鞠躬老板前；
+28  *                   奔驰宝马贵者趣，公交自行程序员。
+29  *                   别人笑我忒疯癫，我笑自己命太贱；
+30  *                   不见满街漂亮妹，哪个归得程序员？
+31 */
--- test/tree.h
+++ test/tree.h
@@ -4,3 +4,119 @@
 using namespace std;
 int tree_head=0;
 
+void sushu()
+{
+	double a;
+	float b;
+	int c;
+	cout << "请依次输入a,b,c" << endl;
+	cin >> a;
+	cin >> b;
+	cin >> c;
+
+	double mix;
+	mix = a + b + c;
+	cout << mix << endl;
+
+}
+
+void shuzu()
+{
+	int tree[] = { 11,2,34,6,23,66,14,77,55,35 };
+
+
+	for (int i = 0; i < 9; i++)
+	{
+		for (int j = 1; j < 9 - i; j++)
+		{
+			if (tree[j] < tree[j - 1])
+			{
+				int temp;
+				temp = tree[j];
+				tree[j] = tree[j - 1];
+				tree[j - 1] = temp;
+			}
+		}
+	}
+	for (int i = 0; i < 9; i++)
+	{
+		cout << tree[i] << endl;
+	}
+}
+
+void koujuebiao()
+{
+	for (int i = 1; i < 10; i++)
+	{
+		for (int j = 1; j <= i; j++)
+		{
+			cout << i << "*" << j << "=" << (j * i) << " ";
+		}
+		cout << endl;
+	}
+}
+
+void shuixianhua()
+{
+	int i = 100;
+	while (i < 10000)
+	{
+		int a = 0;
+		int b = 0;
+		int c = 0;
+
+		a = i % 10;//个位
+		b = i / 10 % 10;//十位
+		c = i / 100;//百位
+
+		if (a * a * a + b * b * b + c * c * c == i)//总和与原数比较
+			cout << i << endl;
+		i++;
+
+
+
+
+	}
+}
+
+void hanoi(int n, char a, char b, char c)//hanoi tower
+{
+	if (n == 1)
+	{
+		cout << "要将" << n << "个圆盘从" << a << "柱子分到" << b << "柱子" << endl;
+
+	}
+	else
+	{
+		hanoi(n - 1, a, c, b);
+		cout << "把第" << n << "个盘子从" << a << "移动到" << c << endl;
+		hanoi(n - 1, b, a, c);
+
+	}
+
+}
+bool isPalindrome(int x) {
+	if (x < 0) {
+		return false;
+	}
+	long i = 0;
+	long n = 0;
+	long b = x;
+	while (x > 0) {			//整数倒转
+		i = x % 10;
+		n = n * 10 + i;
+		x = x / 10;
+	}
+	if (n == b) {
+		return true;
+	}
+	else {
+		return false;
+	}
+}
+void RomanToInt()
+{
+
+
+}
+
